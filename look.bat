2b04d1d19966f0ecdccf66709ebec42abee7f5d6
Andrew Foster <afoster1@gmail.com>
2026-02-04 20:24:12 +0000

    Can now select multiple files with look.bat

--- look.bat
+++ look.bat
@@ -10,6 +10,9 @@ if "%~2"=="" (
     set servernamevar=%2
 )
 
+rem Define how long to wait between opening multiple files
+set gvim_wait_period_secs=10
+
 rem Start of "strLen" macro.
 set LF=^
 
@@ -32,22 +35,40 @@ rem End of "strLen" macro.
 setlocal enabledelayedexpansion
 
 rem Invoke ripgrep, fzf and bat to find the search term
+set "started="
+set "timeout_done="
 @for /F "tokens=* USEBACKQ" %%A in (`rg --ignore-case --color=always --line-number --no-heading %1 ^
-| fzf --ansi --color "hl:-1:underline,hl+:-1:underline:reverse"  --delimiter ":" --preview "bat --color=always --highlight-line {2} {1}" --preview-window "up,border-bottom,+{2}+3/3,~3"`) do @set _input=%%A
-
-rem Ensure the output hasn't got any problematic characters
-rem set "_input=Th""i\s&& is not good _maybe_!~*???"
-set "_output="
-set "map=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890\\.-_:"
-%$strLen% len _input
-for /L %%n in (0 1 %len%) DO (
-    for /F "delims=*~ eol=*" %%C in ("!_input:~%%n,1!") do (
-        if "!map:%%C=!" NEQ "!map!" set "_output=!_output!%%C"
+| fzf -m --ansi --color "hl:-1:underline,hl+:-1:underline:reverse"  --delimiter ":" --preview "bat --color=always --highlight-line {2} {1}" --preview-window "up,border-bottom,+{2}+3/3,~3"`) do (
+    set "_input=%%A"
+    
+    rem Ensure the output hasn't got any problematic characters
+    rem set "_input=Th""i\s&& is not good _maybe_!~*???"
+    set "_output="
+    set "map=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890\\.-_:"
+    %$strLen% len _input
+    for /L %%n in (0 1 !len!) DO (
+        for /F "delims=*~ eol=*" %%C in ("!_input:~%%n,1!") do (
+            if "!map:%%C=!" NEQ "!map!" set "_output=!_output!%%C"
+        )
     )
-)
-
-rem Extract the filename and line number, then run vim.
-@for /F "tokens=1-3 delims=:" %%A in ("%_output%") do ( @set filename=%%A & set line=%%B )
-if not "%filename%"=="" (
-   @call gvim --servername %servernamevar% --remote-tab-wait-silent +%line% "%filename%"
+    
+    rem Extract the filename and line number.
+    for /F "tokens=1-3 delims=:" %%B in ("!_output!") do (
+        set "filename=%%B"
+        set "line=%%C"
+    )
+    
+    if not "!filename!"=="" (
+        if not defined started (
+            set "started=1"
+            call gvim --servername %servernamevar% --remote-tab-silent +!line! "!filename!"
+        ) else (
+	    rem It shouldn't take this long to open gvim.... but it does, sigh.
+            if not defined timeout_done (
+                set "timeout_done=1"
+                timeout /t %gvim_wait_period_secs% /nobreak >nul
+            )
+            call gvim --servername %servernamevar% --remote-tab-wait-silent +!line! "!filename!"
+        )
+    )
 )
