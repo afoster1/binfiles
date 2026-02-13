4738374afb43749354870abaf93c3fb55dd256d2
Andrew Foster <afoster1@gmail.com>
2026-02-13 17:04:28 +0000

    Improved gitlook

--- gitlook.bat
+++ gitlook.bat
@@ -4,13 +4,18 @@ rem 1. The optional filename search term
 
 rem Get the search term
 if "%~1"=="" (
-    set searchtermvar=.*
+    set "searchtermvar="
 ) else (
-    set searchtermvar=%1
+    set "searchtermvar=%1"
 )
 
-setlocal enabledelayedexpansion
+setlocal
 
-for /F "tokens=* USEBACKQ" %%A in (`bash -c "git --no-pager log -v --name-only --date=short --pretty=format:\"%%h %%ad %%an\" | awk '/^[0-9a-f]{7,40}[[:space:]]/ { hash = $1; date = $2; author = substr($0, length($1) + length($2) + 3); next } NF > 0 { print hash, date, author, $0 }'" ^
-| rg --ignore-case --color=never --no-heading %searchtermvar% ^
-| fzf -m --ansi --color "hl:-1:underline,hl+:-1:underline:reverse"  --delimiter " " --bind "shift-up:preview-up,shift-down:preview-down,ctrl-u:preview-page-up,ctrl-d:preview-page-down" --preview "bash -c 'git log --name-only -v {1} -1'" --preview-window "right:40%%,border-left"`) do @echo %%A
+if "%searchtermvar%"=="" (
+    for /F "tokens=* USEBACKQ" %%A in (`bash -c "git --no-pager log -v --name-only --date=short --pretty=format:\"%%h%%x09%%ad%%x09%%an\" | awk 'BEGIN { FS = \"\\t\" } NF == 3 { hash = \$1; date = \$2; author = \$3; next } NF > 0 { print hash, date, author, \$0 }'" 
+        ^| fzf -m --ansi --color "hl:-1:underline,hl+:-1:underline:reverse"  --delimiter " " --bind "shift-up:preview-up,shift-down:preview-down,ctrl-u:preview-page-up,ctrl-d:preview-page-down" --preview "bash -c 'git log --name-only -v {1} -1'" --preview-window "right:40%%,border-left"`) do @echo %%A
+) else (
+    for /F "tokens=* USEBACKQ" %%A in (`bash -c "git --no-pager log -v --name-only --date=short --pretty=format:\"%%h%%x09%%ad%%x09%%an\" | awk 'BEGIN { FS = \"\\t\" } NF == 3 { hash = \$1; date = \$2; author = \$3; next } NF > 0 { print hash, date, author, \$0 }'" 
+        ^| rg --ignore-case --color=never --no-heading %searchtermvar% ^
+        ^| fzf -m --ansi --color "hl:-1:underline,hl+:-1:underline:reverse"  --delimiter " " --bind "shift-up:preview-up,shift-down:preview-down,ctrl-u:preview-page-up,ctrl-d:preview-page-down" --preview "bash -c 'git log --name-only -v {1} -1'" --preview-window "right:40%%,border-left"`) do @echo %%A
+)
